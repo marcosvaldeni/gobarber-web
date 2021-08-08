ff67f0818b9c11e75af69a1d8b32829d380d7466
Marcos Lucas <marcosvaldeni@gmail.com>
2021-08-08 16:28:12 +0100

    Creating Error Tooltip

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
 
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -32,6 +34,12 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+    
+    ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
 
   input {
     flex: 1;
@@ -43,7 +51,25 @@ export const Container = styled.div<ContainerProps>`
       color: #666360;
     }
   }
+
   svg {
     margin-right: 16px;
+  }
+`;
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
