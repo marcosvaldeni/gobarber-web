9e117cee990ac96592b6889d464d0d8644130baf
Marcos Lucas <marcosvaldeni@gmail.com>
2021-08-26 20:00:12 +0100

    Month Availability

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -31,6 +31,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:token');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -47,6 +49,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:token', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
