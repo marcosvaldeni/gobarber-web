35e352af5878274e0b387ca1ae678b2c7bc1900e
Marcos Valdeni Lucas <marcosvaldeni@gmail.com>
2021-10-26 20:12:10 +0100

    profile page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState, useMemo } from 'react';
 import { isToday, format, parseISO, isAfter } from 'date-fns';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
+import { Link } from 'react-router-dom';
 
 import { FiPower, FiClock } from 'react-icons/fi';
 
@@ -144,7 +145,9 @@ const Dashboard: React.FC = () => {
             />
             <div>
               <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,135 @@
+import React, { useCallback, useRef } from 'react';
+import { FiLock, FiUser, FiMail, FiCamera, FiArrowLeft } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import { useToast } from '../../hooks/toast';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { Container, Content, AvatarInput } from './styles';
+import { useAuth } from '../../hooks/auth';
+
+interface ProfileFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const { user } = useAuth();
+
+  const handleSubmit = useCallback(
+    async (data: ProfileFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Name is mandatory!'),
+          email: Yup.string()
+            .email('Type a valid email!')
+            .required('Email is mandatory'),
+          password: Yup.string().min(6, 'At least 6 characters'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+
+        history.push('/');
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado!',
+          description: 'Você já pode fazer seu logon no GoBarber!',
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro at registre',
+          description: 'An erro has happened during the registre, try again.',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <header>
+        <div>
+          <Link to="dashboard">
+            <FiArrowLeft />
+          </Link>
+        </div>
+      </header>
+
+      <Content>
+        <Form
+          onSubmit={handleSubmit}
+          ref={formRef}
+          initialData={{
+            name: user.name,
+            email: user.email,
+          }}
+        >
+          <AvatarInput>
+            <img src={user.avatar_url} alt={user.name} />
+            <button type="button">
+              <FiCamera />
+            </button>
+          </AvatarInput>
+
+          <h1>My Profile</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Name" />
+
+          <Input name="email" icon={FiMail} placeholder="Email" />
+
+          <Input
+            containerStyle={{ marginTop: 24 }}
+            name="old_password"
+            icon={FiLock}
+            type="password"
+            placeholder="Current Password"
+          />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="New Password"
+          />
+
+          <Input
+            name="confirm_password"
+            icon={FiLock}
+            type="password"
+            placeholder="Confirm Password"
+          />
+
+          <Button type="submit">Save Changes</Button>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default Profile;
